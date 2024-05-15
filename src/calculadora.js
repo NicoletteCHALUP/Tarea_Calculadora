0e1fc95e81d63e5f2315c40b32ebd7b720bed1f6
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-05-15 19:09:29 -0400

    Toma en cuenta cadena con dos números: Para una cadena 1,2 -> 3

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,6 +1,9 @@
 function sumarNumeros(cadena) {
+  if (cadena === "") return 0;
+    return cadena.split(",").reduce((total, num) => {
+      const parsedNum = parseInt(num);
+      return isNaN(parsedNum) ? total : total + parsedNum;}, 0);
   
-  return cadena === "" ? 0 : parseInt(cadena);
 }
 
 export default sumarNumeros;
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -4,10 +4,15 @@ describe("Sumar números de cadena", () => {
   it("Debería devolver 0 para cadena vacía", () => {
     expect(sumarNumeros("")).toEqual(0);
   });
-  
+
   it("Debería devolver el número mismo para una cadena con un solo número", () => {
     expect(sumarNumeros("2")).toEqual(2);
   });
+
+  it("Debería devolver la suma de dos números para una cadena con dos números separados por coma", () => {
+    expect(sumarNumeros("1,2")).toEqual(3);
+  });
+
 
 });
 
